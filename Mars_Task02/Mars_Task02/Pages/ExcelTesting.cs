46ef46f2f15813719133e5dd33a75dd7b5f3db45
Nikita <nikita.jindal@live.com>
2023-04-29 18:17:34 +1000

    Test case and conditions added

---
+++ Mars_Task02/Mars_Task02/Pages/ExcelTesting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mars_Task02.Pages
+{
+    [TestClass]
+    public class ExcelTesting
+    {
+        static DataSet mTestData;
+       
+       [ClassInitialize]
+
+        public static void BeforeAllTests(TestContext testContext)
+        {
+            using (var stream = File.Open(@"C:\Users\nikit\OneDrive\Desktop\Book1.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    mTestData =  reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    }
+                    );
+                    
+                }
+                Console.WriteLine(mTestData);
+            }
+        }
+
+        [TestMethod]
+        public void LaunchExcel()
+        {
+
+        }
+    }
+}
---
+++ Mars_Task02/Mars_Task02/Tests/ExcelTest.cs
@@ -0,0 +1,20 @@
+﻿using Mars_Task02.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Task02.Tests
+{
+    
+    public class ExcelTest
+    {
+        [Test]
+        public void Excel()
+        {
+            ExcelTesting excelTestingObj = new ExcelTesting();
+        }
+    }
+}
