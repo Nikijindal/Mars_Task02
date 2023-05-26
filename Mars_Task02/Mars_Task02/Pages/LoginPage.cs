5f06cafaa54fede08fb14a0ac84a937826da8a32
Nikita <nikita.jindal@live.com>
2023-05-26 19:35:01 +1000

    26May

--- Mars_Task02/Mars_Task02/Pages/LoginPage.cs
+++ Mars_Task02/Mars_Task02/Pages/LoginPage.cs
@@ -1,6 +1,7 @@
 ﻿
 using Mars_Task02.Utilities;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
 using System;
@@ -16,20 +17,30 @@ namespace Mars_Task02.Pages
     {
         public LoginPage()
         {
+
             ExcelLib.PopulateInCollection(ExcelPath, "SignIn");
         }
-        private static IWebElement signinButton => driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+        private static IWebElement signInButton => driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
         private static IWebElement usernameTextbox => driver.FindElement(By.Name("email"));
         private static IWebElement passwordTextbox => driver.FindElement(By.Name("password"));
         private static IWebElement loginButton => driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
         private static IWebElement rememberMeCheckbox => driver.FindElement(By.Name("rememberDetails"));
         public void LoginActions()
-        
+
         {
-            //RowNum = 2;
-            signinButton.Click();
-            usernameTextbox.SendKeys(ExcelLib.ReadData(2, "Username"));
-            passwordTextbox.SendKeys(ExcelLib.ReadData(2, "Password"));
+            RowNum = 2;
+            signInButton.Click();
+
+            try
+            {
+                usernameTextbox.SendKeys(ExcelLib.ReadData(RowNum, "Username"));
+                passwordTextbox.SendKeys(ExcelLib.ReadData(RowNum, "Password"));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Username or password is incorrect", ex.Message);
+            } 
+            
             rememberMeCheckbox.Click();
             loginButton.Click();
         }
